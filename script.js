7d6cb9158eb67bf62e57dc294aacf7c38f82dfb3
kritikaverma01 <Kritikav807@gmail.com>
2026-09-24 19:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+const numbers = [1, 2, 3, 4];
+
+// Initial Promise
+new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(numbers);
+  }, 0);
+})
+  .then((arr) => {
+    // Filter even numbers
+    const evenNumbers = arr.filter((num) => num % 2 === 0);
+
+    // Wait 1 second and display [2, 4]
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        output.innerText = evenNumbers;
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then((evenNumbers) => {
+    // Multiply each number by 2
+    const result = evenNumbers.map((num) => num * 2);
+
+    // Wait another 2 seconds and display [4, 8]
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        output.innerText = result;
+        resolve(result);
+      }, 2000);
+    });
+  });
